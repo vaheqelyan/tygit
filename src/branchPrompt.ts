277b7cd37a3ef7ba11ecd06c8ce831bd300cb3fe
vaheqelyan <martinkenner@mail.com>
2018-10-08 15:53:13 +0400

    "Prompt for creating a new branch"

---
+++ src/branchPrompt.ts
@@ -0,0 +1,28 @@
+import Git from "./git";
+import Prompt from "./prompt";
+import Screen from "./screen";
+
+import { Inject, Service } from "typedi";
+
+@Service()
+class BranchPrompt extends Prompt {
+	@Inject(() => Git)
+	public gitFactory: Git;
+	@Inject(() => Screen)
+	public screenFactory: Screen;
+
+	public handle = () => {
+		this.screenFactory.reload(true);
+	};
+	public handleError = err => {
+		this.screenFactory.alertError(err);
+	};
+
+	public onSubmit(branchName) {
+		this.gitFactory.newBranch(branchName, this.handle, this.handleError);
+		this.screenFactory.screen.remove(this.element);
+		this.screenFactory.screen.render();
+	}
+}
+
+export default BranchPrompt;
