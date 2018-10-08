e800962864b613dd178b4e86ce955a9d4e9d2dda
vaheqelyan <martinkenner@mail.com>
2018-10-08 15:47:16 +0400

    "Prompt for merging"

---
+++ src/mergePrompt.ts
@@ -0,0 +1,31 @@
+import { Inject } from "typedi";
+import Git from "./git";
+import MSG from "./messages/statusBar";
+import Prompt from "./prompt";
+import Screen from "./screen";
+import StatusBar from "./statusBar";
+
+class MergePrompt extends Prompt {
+	@Inject(() => Git)
+	private gitFactory: Git;
+	@Inject(() => Screen)
+	private screenFactory: Screen;
+	@Inject(() => StatusBar)
+	private statusBarFactory: StatusBar;
+
+	public onSubmit(value) {
+		this.gitFactory.merge(value, this.handleMerge, this.handleMergeError);
+		this.screenFactory.screen.remove(this.element);
+		this.statusBarFactory.setTitleAndRender(MSG.MERGING, false);
+		this.screenFactory.screen.render();
+	}
+
+	private handleMerge = () => {
+		this.statusBarFactory.toogleContent(MSG.MERGED);
+	};
+
+	private handleMergeError = err => {
+		this.screenFactory.alertError(err);
+	};
+}
+export default MergePrompt;
