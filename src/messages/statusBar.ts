700273571357c2fdb0c0fbc1711766017684758f
vaheqelyan <martinkenner@mail.com>
2018-10-08 15:50:22 +0400

    "Messages for statusbar"

---
+++ src/messages/statusBar.ts
@@ -0,0 +1,20 @@
+
+const MSG = {
+	COMMITED: "Ok::File/s has been commited",
+	COMMITING: "Wait::Commiting",
+	LOADED: "Ok::Loaded",
+	LOADING: "Wait::Loading...",
+	MERGED:"Ok::Merged",
+	MERGING:"Wait:Merging branch",
+	PULLED: "Ok::Pulled",
+	PULLING: "Wait::Pulling...",
+	PUSHING: "Wait:Pushing...",
+	RELOAD: "Wait::Reloading...",
+	RELOADED: "Ok::Reloaded",
+	RELOAD_STATUS: "Wait::Reloading status...",
+	STATUS_RELOADED: "Ok::Status reloaded",
+	TRACKED: "Ok::File/s has been tracked",
+	TRACKING: "Wait::Tracking files",
+};
+
+export default MSG;
