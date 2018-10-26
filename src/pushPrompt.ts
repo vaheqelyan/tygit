35b90d8dcbd7e3114f45cb32525c260fde8ee6f8
vaheqelyan <martinkenner@mail.com>
2018-10-26 17:10:38 +0400

    Fix error handling

--- src/pushPrompt.ts
+++ src/pushPrompt.ts
@@ -1,4 +1,3 @@
-import * as fuzzysearch from "fuzzysearch";
 import { Inject } from "typedi";
 import Git from "./git";
 import MSG from "./messages/statusBar";
@@ -13,24 +12,26 @@ class PushInput extends Prompt {
 	public statusBarFactory: StatusBar;
 	@Inject(() => Git)
 	public gitFactory: Git;
-	public handle = (data: Buffer) => {
-		const str = data.toString();
-		if (fuzzysearch("rejected", str)) {
-			this.screenFactory.alertError(str);
-		} else if (fuzzysearch("fatal", str)) {
-			this.screenFactory.alertError(str);
-		} else {
-			this.statusBarFactory.toggleContent(`Ok::Pushed to ${this.gitFactory.getCurrentBranch()}`);
-		}
-	};
+	private spawnResponse: string;
 	public onSubmit(value) {
-		this.gitFactory.push(value, this.handle);
+		this.spawnResponse = null;
+		this.gitFactory.push(value, this.handle, this.handleSpawnClose);
 
 		this.statusBarFactory.setTitleAndRender(MSG.PUSHING, false);
 
 		this.screenFactory.screen.remove(this.element);
 		this.screenFactory.screen.render();
 	}
+	private handle = (data: Buffer) => {
+		this.spawnResponse = data.toString();
+	};
+	private handleSpawnClose = code => {
+		if (code !== 0) {
+			this.screenFactory.alertError(this.spawnResponse);
+		} else {
+			this.statusBarFactory.toggleContent(MSG.PUSHED);
+		}
+	};
 }
 
 export default PushInput;
