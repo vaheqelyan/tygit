eae7c3c7923b9dd031009762d085f4bcdc1aa7f8
vaheqelyan <martinkenner@mail.com>
2018-11-11 13:38:05 +0400

    Create key.ts

---
+++ src/key.ts
@@ -0,0 +1,28 @@
+abstract class Key {
+	public keyDeb: boolean = false;
+	private keyTime: Date = new Date();
+	private diffTime: number = 0;
+
+	public bindKey = () => {
+		const n = new Date();
+		const differenceTravel = n.getTime() - this.keyTime.getTime();
+		this.diffTime = differenceTravel;
+		setTimeout(() => {
+			if (this.diffTime <= 200) {
+				if (this.keyDeb) {
+					this.onDbKey();
+				}
+				this.keyDeb = !this.keyDeb;
+			} else {
+				this.onKey();
+			}
+		}, 500);
+		this.keyTime = n;
+	};
+
+	protected abstract onDbKey(): void;
+
+	protected abstract onKey(): void;
+}
+
+export default Key;
