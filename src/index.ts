c0c15a318f51bb0b391a7de0b88fc1ee3f7eb628
vaheqelyan <martinkenner@mail.com>
2018-11-11 13:49:48 +0400

    Added version and help commands

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,26 @@
 #!/usr/bin/env node
 import "reflect-metadata";
+import preFn from "./fn/preRun";
 import { ICliOptions } from "./interfaces/cli";
-
-import { Container } from "typedi";
 const argv: ICliOptions = require("minimist")(process.argv.slice(2));
-import { exists, readdir } from "fs";
-import Screen from "./screen";
 
 const [gitPath] = argv._;
+if (argv.version) {
+	console.log(require("../package.json").version);
+	process.exit();
+}
+
+if (argv.help) {
+	console.log(`
+	--version, -v  - Current version
+	--terminal - Set the encode for the terminal
+	--help - To get the help
+	`);
+	process.exit();
+}
+
 if (gitPath) {
-	exists(gitPath, isExists => {
-		if (isExists) {
-			readdir(gitPath, (err, list) => {
-				if (err) {
-					console.error(err);
-				}
-				if (list.indexOf(".git") !== -1) {
-					const getTerm = argv.terminal ? argv.terminal : null;
-					if (typeof getTerm === "boolean") {
-						console.log("Please set the type of terminal encoding");
-					} else {
-						Container.set("terminal", getTerm);
-						Container.set("git-path", gitPath);
-						Container.set("terminal-size", { width: process.stdout.columns, height: process.stdout.rows });
-						const sc = Container.get(Screen);
-						sc.initStateAndRender();
-					}
-				} else {
-					console.log("This is not a git repository");
-				}
-			});
-		} else {
-			console.log("The path is not correct");
-		}
-	});
+	preFn(gitPath, argv);
 } else {
 	console.log("Please, choose git repository path");
 }
